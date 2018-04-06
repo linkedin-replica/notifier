ae980c350f6da8b2fcfbb4d144f216ac227b08b2
Soliman <ahmed.soliman9595@gmail.com>
2018-04-07 00:30:47 +0200

    add save notification in cache call to the command

--- src/main/java/com/linkedin/replica/notifier/commands/impl/SendNotificationCommand.java
+++ src/main/java/com/linkedin/replica/notifier/commands/impl/SendNotificationCommand.java
@@ -1,14 +1,20 @@
 package com.linkedin.replica.notifier.commands.impl;
 
 
+import com.linkedin.replica.notifier.cache.NotifierCacheHandler;
 import com.linkedin.replica.notifier.commands.Command;
 import com.linkedin.replica.notifier.database.handlers.NotificationsHandler;
 import com.linkedin.replica.notifier.models.Notification;
+import com.sun.tools.corba.se.idl.constExpr.Not;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 public class SendNotificationCommand extends Command {
+
+    private NotifierCacheHandler notifierCacheHandler;
+
     public SendNotificationCommand(HashMap<String, Object> args) {
         super(args);
     }
@@ -17,7 +23,7 @@ public class SendNotificationCommand extends Command {
         NotificationsHandler dbHandler = (NotificationsHandler) this.dbHandler;
         // validate that all required arguments are passed
         validateArgs(new String[]{"userId", "text", "link"});
-
+        notifierCacheHandler = (NotifierCacheHandler) cacheHandler;
         String userId = args.get("userId").toString();
         String notificationText = args.get("text").toString();
         String notificationLink = args.get("link").toString();
@@ -30,7 +36,11 @@ public class SendNotificationCommand extends Command {
                         timeStamp,
                         false);
         dbHandler.sendNotification(userId, newNotification);
-
+        try {
+            notifierCacheHandler.saveNotification(userId, newNotification);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
